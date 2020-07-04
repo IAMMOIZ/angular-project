8f41ffb0df343072281021c9f352b52c628642e7
Moiz Ansari <moiz.ansari@microfocus.com>
2020-07-04 17:56:41 +0530

    home and student component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
+//importing neccesory modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 //import components
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { StudentComponent } from './student/student.component'
 @NgModule({
 	imports : [ BrowserModule , FormsModule],
-	declarations : [ AppComponent ],
+	declarations : [ AppComponent , HomeComponent , StudentComponent],
+	
 	bootstrap : [ AppComponent ]
 })
 
